83eed9e095c4318f4ca3727ce21dd877dcca5ec4
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:44:49 +0700

    Initial commit

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+function dataKelas() {
+    var kelasV = {
+        ketua: "Bayu",
+        wakilketua: "Niki",
+        Sekretaris: "Desi dan Mariana",
+        Keamanan: "Dimas dan Bagas",
+        Kebersihan: "Rahmat"
+    }
+    kelasV.Kebersihan = "Rohmanto";
+    console.log(kelasV)
+
+}
+dataKelas()
